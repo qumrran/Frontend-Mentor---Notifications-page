105582183cc727ae8107aea1f8d93739c96cb52d
Piotr Obara <pobara@o2.pl>
2023-09-08 16:21:11 +0200

    Challenge completed

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-const counter = document.querySelector(".commentsSection__heder__counter");
-const marker = document.querySelector(".commentsSection__heder__markout");
-const userBox = document.querySelectorAll(".commentsSection__users");
-const usersBox2 = document.querySelectorAll(".commentsSection__users__box2");
-
-
+const counter = document.querySelector('.commentsSection__heder__counter');
+const marker = document.querySelector('.commentsSection__heder__markout');
+const userBox = document.querySelectorAll('.commentsSection__users');
+const usersBox2 = document.querySelectorAll('.commentsSection__users__box2');
+const redpoints = document.querySelectorAll(
+	'.commentsSection__users__box2__redpoint'
+);
+let innercounter = 0;
 
 function addDeleteRedpoint(event) {
-    const redpoint = event.currentTarget.querySelector(".commentsSection__users__box2__redpoint");
-    const userBox = event.currentTarget;
+	const redpoint = event.currentTarget.querySelector(
+		'.commentsSection__users__box2__redpoint'
+	);
+	const userBox = event.currentTarget;
 
-    if (redpoint.style.display === "inline-block") {
-        redpoint.style.display = "none";
-        userBox.classList.remove("highlighted");
-    } else {
-        redpoint.style.display = "inline-block";
-        userBox.classList.add("highlighted");
-    }
+	if (redpoint.style.display === 'inline-block') {
+		redpoint.style.display = 'none';
+		userBox.classList.remove('highlighted');
+	} else {
+		redpoint.style.display = 'inline-block';
+		userBox.classList.add('highlighted');
+	}
 }
 
 userBox.forEach((element) => {
-    element.addEventListener("click", addDeleteRedpoint);
+	element.addEventListener('click', addDeleteRedpoint);
 });
 
+function deleteAllRedpoints() {
+	userBox.forEach((element) => {
+		element.classList.remove('highlighted');
+	});
 
+	redpoints.forEach((redpoint) => {
+		redpoint.style.display = 'none';
+	});
+
+	innercounter = 0;
+	counter.textContent = '0';
+}
+
+marker.addEventListener('click', deleteAllRedpoints);
+
+document.addEventListener('DOMContentLoaded', function () {
+	userBox.forEach((element, index) => {
+		element.addEventListener('click', () => {
+			if (element.classList.contains('highlighted')) {
+				innercounter++;
+			} else {
+				innercounter--;
+			}
+			counter.textContent = innercounter;
+		});
+	});
+});
